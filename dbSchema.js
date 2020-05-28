f60242247d4a2b1879589ffce8e7895a1a03d12c
Mohd Hussain <samsteve124@gmail.com>
2020-05-28 10:32:40 +0530

    #3 dbSchema onlu use view file no use

---
+++ dbSchema.js
@@ -0,0 +1,12 @@
+// db schema only for representation purpose
+let db = {
+    screams: [
+        {
+            userHandle: 'One',
+            body: 'Body of screams',
+            createdAt: '2020-05-28T04:56:33.251Z',
+            likedCountL :5,
+            commentCount: 3
+        }
+    ]
+}
--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,6 @@
 const functions = require('firebase-functions')
 const admin = require('firebase-admin')
+const app  = require('express')()
 
 // initiliaze firebase app
 admin.initializeApp()
@@ -7,18 +8,25 @@ admin.initializeApp()
 // create firestore instance
 let db = admin.firestore()
 
-exports.getScreams = functions.https.onRequest((req,res ) => {
-    db.collection('screams').get()
+// to have one endpoint for both into one use express
+
+app.get('/screams', (req, res) => {
+    db
+        .collection('screams')
+        .orderBy('createdAt', 'desc')
+        .get()
         .then(data => {
             let screams = []
             data.forEach(doc => {
-                screams.push(doc.data())
+                screams.push({
+                    screamId: doc.id,
+                    ...doc.data()})
             })
             return res.json(screams)
         }).catch(err => console.error(err))
-});
+})
 
-exports.createScreams =  functions.https.onRequest((req, res) => {
+app.post('/screams', (req, res) => {
     const {
         userHandle,
         body,
@@ -26,8 +34,22 @@ exports.createScreams =  functions.https.onRequest((req, res) => {
     } = req.body
 
     db.collection('screams')
-        .add({userHandle,body, createdAt})
+        .add({
+            userHandle,
+            body,
+            createdAt
+        })
         .then(doc => {
-            res.json({message: `New Screams created successfully ${doc.id}`})
-        }).catch(err =>  console.error(err))
+            res.json({
+                message: `New Screams created successfully ${doc.id}`
+            })
+        }).catch(err => {
+            res.status(500).json({
+                error: "Something went wrong"
+            })
+            console.error(err)
+        })
 })
+
+// all in one route using express
+exports.api = functions.https.onRequest(app)
