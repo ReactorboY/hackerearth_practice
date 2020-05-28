625730865b3df7a7ce99dddf3b54515d163e73e7
Mohd Hussain <samsteve124@gmail.com>
2020-05-28 09:57:58 +0530

    #2

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,33 @@
-const functions = require('firebase-functions');
+const functions = require('firebase-functions')
+const admin = require('firebase-admin')
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+// initiliaze firebase app
+admin.initializeApp()
+
+// create firestore instance
+let db = admin.firestore()
+
+exports.getScreams = functions.https.onRequest((req,res ) => {
+    db.collection('screams').get()
+        .then(data => {
+            let screams = []
+            data.forEach(doc => {
+                screams.push(doc.data())
+            })
+            return res.json(screams)
+        }).catch(err => console.error(err))
+});
+
+exports.createScreams =  functions.https.onRequest((req, res) => {
+    const {
+        userHandle,
+        body,
+        createdAt = new Date().toISOString()
+    } = req.body
+
+    db.collection('screams')
+        .add({userHandle,body, createdAt})
+        .then(doc => {
+            res.json({message: `New Screams created successfully ${doc.id}`})
+        }).catch(err =>  console.error(err))
+})
